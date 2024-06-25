4f25dde95cd35c7cdefe23204acf763c71b260f9
Robert Krahn <robert.krahn@gmail.com>
2024-06-25 14:41:49 +0200

    add nix setup

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              rustc
+              cargo
+              clippy
+              pkg-config
+            ];
+
+            buildInputs = with pkgs; [
+              openssl
+              clang
+            ] ++ (if pkgs.stdenv.isDarwin then [ libiconv ] else [ ]);
+
+            packages = with pkgs; [
+              rust-analyzer
+              rustfmt
+            ];
+
+            RUST_BACKTRACE = "1";
+            RUST_LOG = "debug";
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          };
+        }
+    );
+}
